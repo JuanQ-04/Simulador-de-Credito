d7269199b98cb137a8f440d41a1922009cebcf9b
JuanQ-04 <jquinone11@cuc.edu.co>
2025-03-29 10:11:24 -0500

    Agregar archivos de proyecto.

---
+++ simulador de credito/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(simulador_de_credito.Startup))]
+namespace simulador_de_credito
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
